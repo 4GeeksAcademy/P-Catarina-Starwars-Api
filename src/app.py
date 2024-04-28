983b44ee21004d8942428a8ffcdeffd3d05aa584
P-Catarina <155997817+P-Catarina@users.noreply.github.com>
2024-04-28 20:31:42 +0000

    starwars rest api: get, post, delete

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -36,7 +36,7 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
+@app.route('/hello', methods=['GET'])
 def handle_hello():
 
     response_body = {
@@ -44,6 +44,140 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+# USERS ROUTES
+@app.route('/users', methods=['GET'])
+def get_table_users():
+    users = User.query.all()
+    users = list(map(lambda user: user.serialize(), users))
+    if len(users) == 0:
+        return "Users table is empty", 400
+    return jsonify(users), 200
+
+# FAVORITES ROUTES
+@app.route('/favorites/users', methods=['GET'])
+def get_table_favorites():
+    favorites = Favorites.query.all()
+    favorites = list(map(lambda fav: fav.serialize(), favorites))
+    if len(favorites) == 0:
+        return "Favorites table is empty", 400
+    return jsonify(favorites), 200
+
+@app.route('/favorites/user-<int:id_user>', methods=['GET'])
+def get_user_favorites(id_user):
+    favorites = Favorites.query.filter_by(id_user = id_user).all()
+    favorites = list(map(lambda fav: fav.serialize(), favorites))
+    if len(favorites) == 0:
+        return "This user doesn't exist or has no favorites yet", 400
+    return jsonify(favorites), 200
+
+@app.route('/favorites/user-<int:id_user>/planet-<int:id_planet>', methods=['POST'])
+def add_favorite_planet(id_user, id_planet):
+    if User.query.get(id_user) is None:
+        return "This user doesn't exist", 400
+    
+    if Planets.query.get(id_planet) is None:
+        return "This planet doesn't exist", 400
+    
+    match = Favorites.query.filter_by(id_user = id_user, id_planet = id_planet).all()
+    if len(match) is not 0:
+        return "Already in the database", 400
+
+    new_fav = Favorites(
+        id_planet = id_planet, 
+        id_user = id_user,
+    )
+
+    db.session.add(new_fav)
+    db.session.commit()
+    return "Planet added to Favorites", 200
+
+@app.route('/favorites/user-<int:id_user>/people-<int:id_person>', methods=['POST'])
+def add_favorite_person(id_user, id_person):
+    if User.query.get(id_user) is None:
+        return "This user doesn't exist", 400
+    
+    if People.query.get(id_person) is None:
+        return "This person doesn't exist", 400
+    
+    match = Favorites.query.filter_by(id_user = id_user, id_person = id_person).all()
+    if len(match) is not 0:
+        return "Already in the database", 400
+
+    new_fav = Favorites(
+        id_person = id_person, 
+        id_user = id_user,
+    )
+
+    db.session.add(new_fav)
+    db.session.commit()
+    return "Person added to Favorites", 200
+
+@app.route('/favorites/user-<int:id_user>/planet-<int:id_planet>', methods=['DELETE'])
+def delete_favorite_planet(id_user, id_planet):
+    if User.query.get(id_user) is None:
+        return "This user doesn't exist", 400
+    
+    if Planets.query.get(id_planet) is None:
+        return "This planet doesn't exist", 400
+    
+    match = Favorites.query.filter_by(id_user = id_user, id_planet = id_planet).first()
+    if match is None:
+        return "Not in Favorites", 400
+
+    db.session.delete(match)
+    db.session.commit()
+    return "Planet removed from Favorites", 200
+
+@app.route('/favorites/user-<int:id_user>/people-<int:id_person>', methods=['DELETE'])
+def delete_favorite_person(id_user, id_person):
+    if User.query.get(id_user) is None:
+        return "This user doesn't exist", 400
+    
+    if People.query.get(id_person) is None:
+        return "This person doesn't exist", 400
+    
+    match = Favorites.query.filter_by(id_user = id_user, id_person = id_person).first()
+    if match is None:
+        return "Not in Favorites", 400
+
+    db.session.delete(match)
+    db.session.commit()
+    return "Person removed from Favorites", 200
+
+# CHARACTERS ROUTES
+@app.route('/people', methods=['GET'])
+def get_table_people():
+    people = People.query.all()
+    people = list(map(lambda person: person.serialize(), people))
+    if len(people) == 0:
+        return "People table is empty", 400
+    return jsonify(people), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_one_person(people_id):
+    person = People.query.get(people_id)
+    person = person.serialize()
+    if person is None:
+        return "This person doesn't exist", 400
+    return jsonify(person), 200
+
+# PLANETS ROUTES
+@app.route('/planets', methods=['GET'])
+def get_table_planets():
+    planets = Planets.query.all()
+    planets = list(map(lambda planet: planet.serialize(), planets))
+    if len(planets) == 0:
+        return "Planets table is empty", 400
+    return jsonify(planets), 200
+
+@app.route('/planets/<int:planets_id>', methods=['GET'])
+def get_one_planet(planets_id):
+    planet = Planets.query.get(planets_id)
+    planet = planet.serialize()
+    if planet is None:
+        return "This planet doesn't exist", 400
+    return jsonify(planet), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
     def serialize(self):
         return {
@@ -17,3 +17,70 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    birth_year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "description": self.description
+        }
+    
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(100), nullable=False)
+    terrain = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "terrain": self.terrain
+        }
+    
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id_person = db.Column(db.Integer, db.ForeignKey('people.id'))
+    id_planet = db.Column(db.Integer, db.ForeignKey('planets.id'))
+
+    user = db.relationship(User)
+    people = db.relationship(People)
+    planets = db.relationship(Planets)
+
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        if self.id_person is not None:
+            return {
+                "id": self.id,
+                "user": self.id_user,
+                "person": self.id_person,
+            }
+    
+        if self.id_planet is not None:
+            return {
+                "id": self.id,
+                "user": self.id_user,
+                "planet": self.id_planet
+            }
